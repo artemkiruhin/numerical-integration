5c7be91d400b77a773f92a874730517ad60a8adf
artemkiruhin <temkaunik@gmail.com>
2025-02-01 20:24:39 +0300

    Anylizer reference fix

--- NumericalIntegration.Frontend/MauiProgram.cs
+++ NumericalIntegration.Frontend/MauiProgram.cs
@@ -17,7 +17,7 @@ public static class MauiProgram
             .ConfigureFonts(fonts => { fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular"); });
 
         builder.Services.AddMauiBlazorWebView();
-        builder.Services.AddSingleton<IntegrationAnalyzer>();
+        builder.Services.AddSingleton<IntegrationLeastSquaresAnalyzer>();
 
 #if DEBUG
         builder.Services.AddBlazorWebViewDeveloperTools();
