bd9918b57920ffbfdd4aa36b69e0271808b9b43a
artemkiruhin <temkaunik@gmail.com>
2025-02-10 11:38:34 +0300

    Frontend main page created

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,47 +1,12 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="container mt-4">
+    <nav class="nav nav-tabs mb-4">
+      <router-link to="/" class="nav-link">Методы интегрирования</router-link>
+      <router-link to="/compare" class="nav-link">Сравнение методов</router-link>
+      <router-link to="/error-estimation" class="nav-link">Оценка погрешности</router-link>
+      <router-link to="/simple-functions" class="nav-link">Простые функции</router-link>
+    </nav>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <router-view></router-view>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,20 @@
-import './assets/main.css'
-
+// main.js
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import IntegrationMethods from './components/IntegrationMethods.vue'
+import CompareMethods from './components/CompareMethods.vue'
+import ErrorEstimation from './components/ErrorEstimation.vue'
+import SimpleFunctionsComparison from './components/SimpleFunctionsComparison.vue'
 
-createApp(App).mount('#app')
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', component: IntegrationMethods },
+        { path: '/compare', component: CompareMethods },
+        { path: '/error-estimation', component: ErrorEstimation },
+        { path: '/simple-functions', component: SimpleFunctionsComparison }
+    ]
+})
+
+createApp(App).use(router).mount('#app')
