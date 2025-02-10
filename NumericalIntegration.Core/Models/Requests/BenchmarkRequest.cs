b3a4f7ad69807ad56459bbf2b776f96e80e523a9
artemkiruhin <temkaunik@gmail.com>
2025-02-10 09:50:31 +0300

    Request models created

---
+++ NumericalIntegration.Core/Models/Requests/BenchmarkRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace NumericalIntegration.Core.Models.Requests;
+
+public class BenchmarkRequest
+{
+    public string Function { get; set; }
+    public double A { get; set; }
+    public double B { get; set; }
+    public int[] SegmentsArray { get; set; }
+}
---
+++ NumericalIntegration.Core/Models/Requests/CalculationRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace NumericalIntegration.Core.Models.Requests;
+
+public class CalculationRequest
+{
+    public string Function { get; set; }
+    public double A { get; set; }
+    public double B { get; set; }
+    public string Method { get; set; }
+    public int? Segments { get; set; }
+}
